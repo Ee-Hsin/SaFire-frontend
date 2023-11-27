bff75dc2a8bdeb8ec841a9055a9d50be9e542f35
Bokang Keith Mogomotsi <bigbk75@gmail.com>
2023-11-27 18:41:57 -0500

    Potential RTC implementation for VideoStreaming
    Still checking

---
+++ VideoView.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect } from "react";
+
+const VideoView = ({ socket }) => {
+  useEffect(() => {
+    const configuration = {
+      iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
+    };
+
+    let peerConnection;
+
+    socket.on("message", async (event) => {
+      const message = event.data;
+
+      if (message.type === "offer") {
+        peerConnection = new RTCPeerConnection(configuration);
+        await peerConnection.setRemoteDescription(
+          new RTCSessionDescription(message.sdp)
+        );
+        const answer = await peerConnection.createAnswer();
+        await peerConnection.setLocalDescription(answer);
+
+        socket.send({
+          type: "answer",
+          sdp: peerConnection.localDescription,
+        });
+      } else if (message.type === "candidate") {
+        await peerConnection.addIceCandidate(
+          new RTCIceCandidate(message.candidate)
+        );
+      }
+    });
+
+    let remoteVideo;
+
+    const setupPeerConnection = (event) => {
+      const stream = event.streams[0];
+      //sets the video source to the remote video stream
+      remoteVideo.srcObject = stream;
+    };
+
+    peerConnection = new RTCPeerConnection(configuration);
+    peerConnection.onicecandidate = (event) => {
+      if (event.candidate) {
+        socket.send({ type: "candidate", candidate: event.candidate });
+      }
+    };
+    peerConnection.ontrack = setupPeerConnection;
+
+    remoteVideo = document.getElementById("remoteVideo");
+  }, []);
+
+  return (
+    <div>
+      <video id="remoteVideo" autoPlay></video>
+    </div>
+  );
+};
+
+export default VideoView;
